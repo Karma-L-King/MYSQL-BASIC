9f321e03c686bdcee003d2e28854819162ecf843
Karma King <2114747@talnet.nl>
2020-06-04 02:34:17 +0200

    nice man

--- 3-Delete/taak02/antwoorden.sql
+++ 3-Delete/taak02/antwoorden.sql
@@ -1,5 +1,9 @@
 -- Opdracht 3
-
+SELECT * FROM `players` WHERE id="198329"
 -- Opdracht 4
 
 -- Opdracht 5
+DELETE FROM players WHERE id = "198329"
+DELETE FROM players WHERE id= "199304"
+DELETE FROM players WHERE id= "207494"
+DELETE FROM players WHERE id= "226753"
